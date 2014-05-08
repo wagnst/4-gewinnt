8f59486f3e2d53ef8efa7e04a10cdb6484b3b157
n2code <n2code@herr-der-mails.de>
2014-05-08 21:17:10 +0200

    board: added first implementation

---
+++ board.c
@@ -0,0 +1,37 @@
+﻿#pragma once
+//Board-Core: Create board, read&update field
+
+struct board myboard;
+
+int newBoard(struct board* target, unsigned int width, unsigned int height)
+{
+	//sanity check
+	if (width<4 || height<4) {
+		return 0;
+	}
+
+	//Initialize board
+	target->width = width;
+	target->height = height;
+	target->numberOfFields = width*height;
+	target->content = malloc(target->numberOfFields * sizeof(char));
+
+	//abort if memory allocation failed
+	if (target->content == NULL) {
+		return 0;
+	}
+
+	//board is ready, now clear it
+	clearBoard(target);
+}
+
+void clearBoard(struct board* target) {
+	//fills the whole board with space chars
+	int i = 0;
+	while (i < target->numberOfFields) {
+		char* field = (target->content+i);
+		*field = 'p';
+		printf("%i:%c\n",(int)field,*field);
+		i++;
+	}
+}
