dc2a19f5300557a4c983c1590317d824c98359e7
d059727 <steffen.tobias.wagner@sap.com>
2014-05-15 15:14:32 +0200

    added mask of used variables

--- variables.h
+++ variables.h
@@ -21,3 +21,11 @@ extern char name1[20];
 extern char name2[20];
 extern char winner[20];
 extern char looser[20];
+extern struct board gameField;
+extern int gameFieldWidth;
+extern int gameFieldHeigth;
+extern int gameFieldCreated;
+extern int playersTurn; //player1 starts the game
+extern int coinPosition; //where the coin is actually placed
+extern char playersCoin[1]; //contains X or O
+
