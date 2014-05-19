a39527691c57ae4d131eba14b78469ee86b93a98
n2code <n2code@users.noreply.github.com>
2014-05-19 20:17:04 +0200

    demo implementation of output buffer

--- mainMenu.c
+++ mainMenu.c
@@ -5,26 +5,27 @@
 #include <conio.h>
 
 void drawMainMenu(int pPos){
-    consoleClear();
- switch(pPos){
-    case 0:
-        output("-->NEW GAME<--\nSETTINGS\nRULES\nHALL OF SHAME\nQUIT\n");
-        break;
-    case 1:
-        output("NEW GAME\n-->SETTINGS<--\nRULES\nHALL OF SHAME\nQUIT\n");
-        break;
-    case 2:
-        output("NEW GAME\nSETTINGS\n-->RULES<--\nHALL OF SHAME\nQUIT\n");
-        break;
-    case 3:
-        output("NEW GAME\nSETTINGS\nRULES\n-->HALL OF SHAME<--\nQUIT\n");
-        break;
-    case 4:
-        output("NEW GAME\nSETTINGS\nRULES\nHALL OF SHAME\n-->QUIT<--\n");
-        break;
-    default:
-    	break;
- }
+	emptyBuffer();
+	switch(pPos){
+		case 0:
+			output("-->NEW GAME<--\nSETTINGS\nRULES\nHALL OF SHAME\nQUIT\n");
+			break;
+		case 1:
+			output("NEW GAME\n-->SETTINGS<--\nRULES\nHALL OF SHAME\nQUIT\n");
+			break;
+		case 2:
+			output("NEW GAME\nSETTINGS\n-->RULES<--\nHALL OF SHAME\nQUIT\n");
+			break;
+		case 3:
+			output("NEW GAME\nSETTINGS\nRULES\n-->HALL OF SHAME<--\nQUIT\n");
+			break;
+		case 4:
+			output("NEW GAME\nSETTINGS\nRULES\nHALL OF SHAME\n-->QUIT<--\n");
+			break;
+		default:
+			break;
+	}
+	flushBuffer();
 }
 
 int mainMenu(){
@@ -35,8 +36,6 @@ int mainMenu(){
     // menu loop
 	while(con){
 		drawMainMenu(pPos);
-		//Windows implementation:
-		#ifdef _WIN32
 		switch(getch()) {
 			case 72:    // key up
 				pPos--;
@@ -54,24 +53,7 @@ int mainMenu(){
 			case 27: //escape
 				exit(0);
 		}
-		//other systems
-		#else
-		scanf("%c", &userInput);
-		switch(userInput){
-			//dummy for key up
-			case 'w':
-				pPos--;
-				break;
-			//dummy for key down
-			case 's':
-				pPos++;
-				break;
-			//dummy for return key
-			case 'e':
-				mainMenuReactToEnter(pPos);
-				break;
-		}
-		#endif
+		fflush(stdin);
 		if(pPos>4){
 			//cursor is "below" the menu-->get him back
 			pPos=0;
@@ -81,36 +63,34 @@ int mainMenu(){
 		}
 		output("\n");
     }
+    return 0;
 }
 
 void mainMenuReactToEnter(int pPos){
+	emptyBuffer();
 	switch(pPos){
 		case 0:
-			consoleClear();
-			//replace printf with corresponding call
-			//output("Starting new Game(Dummy)\nPress any key to return to main\n");
 			gameFunction();
 			con=0;
 			break;
 		case 1:
-			 consoleClear();
 			//replace printf with corresponding call
 			output("Settings Menu(Dummy)\nPress any key to return to main\n");
+			flushBuffer();
+			getch();
 			break;
 		case 2:
-			 consoleClear();
 			//replace printf with corresponding call
 			output("Rules (Dummy)\nPress any key to return to main\n");
+			flushBuffer();
+			getch();
 			break;
 		case 3:
-			 consoleClear();
-			//replace printf with corresponding call
-		    //output("Hall of Shame (Dummy)\nPress any key to return to main\n");
 			showHallOfShame();
 			break;
 		case 4:
 			//end game
 			con=0;
 			exit(0);
-		}
+	}
 }
