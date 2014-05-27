93e6516c4622a03b08e6a9f864f61caba282f45f
n2code <n2code@users.noreply.github.com>
2014-05-27 20:43:47 +0200

    introduced windows launcher (changes window size, title, color and codepage)

---
+++ winlauncher.c
@@ -0,0 +1,11 @@
+﻿#include <stdlib.h>
+
+int main()
+{
+	system("TITLE ConnectFour"); //Titel in Fensterleiste
+	system("CHCP 65001 > nul"); //UTF8 Codepage
+	system("COLOR 0A"); //Grüne Schrift auf schwarzem Grund
+	system("mode con:cols=80 lines=40"); //Fenstergröße
+	system("CLS"); //Konsole leeren
+	return system("4-gewinnt"); //eigentliches Programm starten
+}
