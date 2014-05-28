50e00c97388887a256514dc11962baf1ab0bf9fa
n2code <n2code@users.noreply.github.com>
2014-05-28 18:33:40 +0200

    credit changes

--- credits.c
+++ credits.c
@@ -6,7 +6,7 @@
 */
 void showCredits() {
 	startBuffer(70);
-	output("[ CREDITS ]:\n\n Niko Berkmann | @n2code\n   -> mail@n2code.de\n Steffen Wagner | @wagnst\   -> mail@wagnst.de\n Marvin Klose | @marvinklose\n   -> marvinklose@gmx.net\n Michael Wieneke | @momoxd\n   -> mail@momoxd.de\n Inga Miadowicz | @IngaMiad\n   -> inga.miadowicz@gmx.de\n\n");
+	output("[ CREDITS ]:\n\n Niko Berkmann | @n2code\n   -> n2code@users.noreply.github.com\n Steffen Wagner | @wagnst\n   -> mail@wagnst.de\n Marvin Klose | @marvinklose\n   -> marvinklose@gmx.net\n Michael Wieneke | @momoxd\n   -> mail@momoxd.de\n Inga Miadowicz | @IngaMiad\n   -> inga.miadowicz@gmx.de\n\n");
 	output("Press any key to return to main menu.\n");
 	flushBuffer();
 	getch();
